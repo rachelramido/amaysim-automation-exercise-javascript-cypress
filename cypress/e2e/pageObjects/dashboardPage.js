b7720098f1710322cdbafc04dacada1e575aee68
Hans Dushanthakumar <hansrd_98@yahoo.com>
2022-08-26 20:02:23 +1000

    Handle the mixed content error or redirect to http

--- cypress/e2e/pageObjects/dashboardPage.js
+++ cypress/e2e/pageObjects/dashboardPage.js
@@ -14,7 +14,15 @@ class DashboardPage extends BasePage {
 
   viewServiceDetails() {
     cy.get(this.serviceTile)
-      .should('be.visible')
+    .should('be.visible')
+
+    // This is to handle the mixed content error in firefox as there is a redirect to a http url
+    cy.intercept('GET', /https:\/\/www.*\.amaysim\.com\.au\/my-account\/oauth\/callback\?code.*/, (req) => {
+      req.on('response', (res) => {
+        res.headers.location = res.headers.location.replace('http:', 'https:')
+      })
+    })
+    
     cy.get(this.serviceTile)
       .click()
     mobilePlanDetailsPage.isDisplayed()
